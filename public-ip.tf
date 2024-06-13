284800131bde7399bc87f9408c6bdb2a56f2655d
Anil Kumar <anjena>
2024-06-13 12:41:41 +0530

    modified allocation type from static to dynamic

--- public-ip.tf
+++ public-ip.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "mypublicIP" {
     name = var.ipaddressname
     resource_group_name = azurerm_resource_group.myTerraformGroup.name
     location = var.location
-    allocation_method = "static"
+    allocation_method = "Dynamic"
     tags = var.tags
   
 }
